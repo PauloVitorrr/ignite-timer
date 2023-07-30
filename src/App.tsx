d352e5188248c32f6ec90f3de064f561b7455cc4
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-07-30 18:16:55 -0300

    feat: layout rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Button } from "./components/Button";
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default";
+import {BrowserRouter} from 'react-router-dom'
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
+
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="danger"/>
-      <Button variant="sucess"/>
-      <Button/>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
       <GlobalStyle/>
     </ThemeProvider>
   )
