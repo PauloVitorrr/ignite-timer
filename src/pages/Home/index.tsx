39a7436273326f78ba5613b1126a02e721ca1477
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-08-04 14:49:12 -0300

    feat: aprimorando inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,14 +7,24 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou Trabalhar em</label>
-          <TaskInput id="task" placeholder='Dê um nome para o seu projeto'/>
+          <TaskInput id="task" list="task-suggestions" placeholder='Dê um nome para o seu projeto'/>
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"/>
+            <option value="Projeto 2"/>
+            <option value="Proejto 3"/>
+            <option value="Banana"/>
+          </datalist>
 
           <label htmlFor="MinutesAmount">Durante</label>
           <MinutesAmountInput 
             type="number" 
             id="minutesAmount"
             placeholder='00'
-            />
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
