9a8dc3be9b62a0fdb41e05afcc6774c76049c3a1
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-08-04 17:14:32 -0300

    feat: components status

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -57,5 +57,28 @@ export const HistoryList = styled.div`
             padding-right: 1.5rem
         }
     }
+}
+`
+const STATUS_COLOR = {
+    yellow: 'yellow-500',
+    green: 'green-500',
+    red: 'red-500'
+} as const
+
+interface StatusColor{
+    statusColor: keyof typeof STATUS_COLOR;
+}
+
+export const Status = styled.span<StatusColor>`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    &::before{
+        content: '';
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 50%;
+        background-color: ${props => props.theme[STATUS_COLOR[props.statusColor]]};
     }
 `
