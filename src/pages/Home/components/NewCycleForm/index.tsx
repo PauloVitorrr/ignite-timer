91742ad06b52540d965a27a08709f16629178c8b
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-10-16 08:12:31 -0300

    refactor: separando components

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,37 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
+
+export function NewCycleForm() {
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou Trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register("task")}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Proejto 3" />
+        <option value="Banana" />
+      </datalist>
+
+      <label htmlFor="MinutesAmount">Durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register("minutesAmount", { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  );
+}
